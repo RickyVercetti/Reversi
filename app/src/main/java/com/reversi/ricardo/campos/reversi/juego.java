4f56dec28ed8bbaeffbdc4b3839452a5eea29137
RickyVercetti <ricky_kampos@hotmail.es>
2016-10-21 12:13:22 +0200

    Añadido contenido dinamico (un solo boton)

--- app/src/main/java/com/reversi/ricardo/campos/reversi/juego.java
+++ app/src/main/java/com/reversi/ricardo/campos/reversi/juego.java
@@ -10,4 +10,25 @@ public class juego extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
     }
+    /*protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //crea un layout por codigo
+        LinearLayout layout_principal = new LinearLayout (this);
+        //le da formato
+        layout_principal.setOrientation(LinearLayout.VERTICAL);
+        //Se crea un control de forma dinámica y se añade al layout
+        TextView tv = new TextView(this);
+        tv.setText("TextView en el layout principal!");
+        tv.setGravity(Gravity.CENTER_HORIZONTAL);
+        tv.setBackgroundColor(Color.rgb(170, 00, 00));
+        layout_principal.addView(tv);
+        //Añadimos un layout secundario creado desde recurso
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View layout_secundario=inflater.inflate(R.layout.layout_secundario, (ViewGroup) layout_principal,false);
+        layout_principal.addView(layout_secundario);
+
+        this.setContentView(layout_principal);
+    }*/
+
+
 }
--- app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
+++ app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
@@ -2,12 +2,24 @@ package com.reversi.ricardo.campos.reversi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class principal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_juego);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        RelativeLayout layout_secundario=(RelativeLayout) inflater.inflate(R.layout.activity_juego, null,false);
+        LinearLayout dinamico = (LinearLayout) layout_secundario.findViewById(R.id.dinamico);
+        Button boton = new Button(this);
+        boton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        dinamico.addView(boton);
+        setContentView(layout_secundario);
     }
 }
