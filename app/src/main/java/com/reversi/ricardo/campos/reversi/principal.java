6916a545093e3883bf30eda4ba59c3af81465f23
RickyVercetti <ricky_kampos@hotmail.es>
2016-10-21 11:05:00 +0200

    añadido contenido en pantalla juego 0 errores

--- app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
+++ app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
@@ -8,6 +8,6 @@ public class principal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
+        setContentView(R.layout.activity_juego);
     }
 }
