556daca0311820e297b55c7dea3656db7b593ea6
RickyVercetti <ricky_kampos@hotmail.es>
2016-10-25 08:42:52 +0200

    Añadido dos botones dinamicos,falta cambiarlos por layouts

--- app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
+++ app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
@@ -12,14 +12,27 @@ import android.widget.RelativeLayout;
 public class principal extends AppCompatActivity {
 
     @Override
+    //protected int casillas=7;
+
+    //TODO  Cambiar los botones por layouts dentro de un for y luego añadir los botones
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LayoutInflater inflater = LayoutInflater.from(this);
         RelativeLayout layout_secundario=(RelativeLayout) inflater.inflate(R.layout.activity_juego, null,false);
         LinearLayout dinamico = (LinearLayout) layout_secundario.findViewById(R.id.dinamico);
         Button boton = new Button(this);
-        boton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        boton.setText("HOLA");
+        boton.setWidth(20);
+        boton.setHeight(20);
+        //boton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         dinamico.addView(boton);
+        Button boton1 = new Button(this);
+
+        boton1.setText("ADIOS");
+        boton1.setWidth(20);
+        boton1.setHeight(20);
+        //boton1 .setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        dinamico.addView(boton1);
         setContentView(layout_secundario);
     }
 }
