ecb323c736be6ee0805f966518d1456fb02d6a83
RickyVercetti <ricky_kampos@hotmail.es>
2016-11-22 10:49:00 +0100

    creado pantalla inicial, comentado funcion de vibrar y hacer llamadas a otros activitys a traves de intent

--- app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
+++ app/src/main/java/com/reversi/ricardo/campos/reversi/principal.java
@@ -1,5 +1,7 @@
 package com.reversi.ricardo.campos.reversi;
 
+import android.content.Context;
+import android.os.Vibrator;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,22 +15,23 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class principal extends AppCompatActivity {
 
 
-    private static int FACIL = 6;
-    private static int MEDIO = 8;
-    private static int DIFICIL = 10;
+    private static int TAM = 8;
 
     private static int botonTAG = 0;
+    //TODO linea para insertar vibración, falta insertar en el manifest
+    //Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    //vibrator.vibrate(100); //Dentro del click
 
-
-    //TODO  Cambiar los botones por layouts dentro de un for y luego añadir los botones
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //llamar a juego
-        //TODO hacer que funcione el toast y llevar a la pantalla juego
+        //TODO hacer que cambie de un activity a otro con un Intent
         Toast.makeText(this, "A Jugar!", Toast.LENGTH_SHORT).show();
 
         LayoutInflater inflater = LayoutInflater.from(this);
@@ -39,27 +42,68 @@ public class principal extends AppCompatActivity {
         LinearLayout.LayoutParams configuracion = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         configuracion.setMargins(0,0,0,0);
         configuracion.weight=1;
-
-        for (int i = 0; i < DIFICIL; i++) {
+        Button boton;
+        for (int i = 0; i < TAM; i++) {
             LinearLayout filabotones = new LinearLayout(this);
             filabotones.setOrientation(LinearLayout.HORIZONTAL);
-
+            int mitad = TAM/2;
             filabotones.setLayoutParams(configuracion);
-            for (int j = 0; j < DIFICIL; j++) {
-                Button boton  = new Button(this);
+            for (int j = 0; j < TAM; j++) {
+                boton  = new Button(this);
                 boton.setTag(botonTAG);
+                tableroInicial(i,j,mitad,boton);
                 boton.setLayoutParams(configuracion);
                 filabotones.addView(boton);
-                casillaPermitida(i,j,botonTAG);
+                //casillaPermitida(i,j,botonTAG);
                 botonTAG++;
             }
             dinamico.addView(filabotones);
         }
         setContentView(layout_secundario);
-
-
     }
-    public boolean casillaPermitida(int i, int j, int dir)
+    public void tableroInicial(int i, int j,int mitad,Button boton)
+    {
+        if (j == mitad && i == mitad)
+        {
+            boton.setText("X");
+        }
+        if (j == mitad-1 && i ==mitad-1)
+        {
+            boton.setText("X");
+        }
+        if (j == mitad && i == mitad-1)
+        {
+            boton.setText("O");
+        }
+        if (j ==mitad-1 && i == mitad)
+        {
+            boton.setText("O");
+        }
+    }
+    public View.OnClickListener botonesJuego = new View.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            ((Button) v).setText("X");
+        }
+    };
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.jugar) {
+            //llamar a juego
+            //TODO hacer que funcione el toast y llevar a la pantalla juego
+            Toast.makeText(this, "A Jugar!", Toast.LENGTH_SHORT).show();
+
+
+
+        }
+    }
+    /*public boolean casillaPermitida(int i, int j, int dir)
     {
         NO
         {
@@ -104,22 +148,5 @@ public class principal extends AppCompatActivity {
     public boolean buscarCasillaDireccion()
     {
 
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return true;
-    }
-
-    public void onClick(View v) {
-        if (v.getId() == R.id.jugar) {
-            //llamar a juego
-            //TODO hacer que funcione el toast y llevar a la pantalla juego
-            Toast.makeText(this, "A Jugar!", Toast.LENGTH_SHORT).show();
-
-
-
-        }
-
-    }
+    }*/
 }
