950c5b9ecf75ddf7423f084604ba1a5e5fc2e482
Ricado <ricky_kampos@hotmail.es>
2017-02-02 09:20:28 +0100

    añadiendo pantallas y los intent

--- app/src/main/java/com/reversi/ricardo/campos/reversi/menuInicio.java
+++ app/src/main/java/com/reversi/ricardo/campos/reversi/menuInicio.java
@@ -1,7 +1,10 @@
 package com.reversi.ricardo.campos.reversi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class menuInicio extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class menuInicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inicio);
+    }
+
+
+    public void btn_jugarOnClick(View view) {
+
+        Intent intent = new Intent(this, principal.class);
+        startActivity(intent);
+    }
+
+    /*
+    public void btn_instruccionesOnClick(View view) {
+
+        Intent intent = new Intent(this, instrucciones.class);
+        startActivity(intent);
+    }
+
+    public void btn_puntuacionesOnClick(View view) {
+
+        Intent intent = new Intent(this, puntuaciones.class);
+        startActivity(intent);
+    }
+    */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_inicio, menu);
+        return true;
     }
 }
